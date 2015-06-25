a07280ee701f2ebef5616ef9d72e6427560b822d
Guest User <student@epicodus.com>
2015-06-25 10:15:57 -0700

    Display image on pokemon page

--- app/routes/pokemon.js
+++ app/routes/pokemon.js
@@ -4,16 +4,15 @@ export default Ember.Route.extend({
   model: function(params) {
     var url = 'http://pokeapi.co/api/v1/pokemon/' + params.pkmn_id;
     return $.getJSON(url).then(function(responseJSON){
-      console.log(responseJSON.sprites[0].resource_uri)
-      // return responseJSON;
-      var name = responseJSON['descriptions'][0]['name'];
-      var image = $.getJSON(url).then(function(responseJSON){
-        return "http://pokeapi.co/media/img/19.png"
+
+      var imageUri = 'http://pokeapi.co' + responseJSON['sprites'][0]['resource_uri']
+      var modelObject = {}
+
+      $.getJSON(imageUri).then(function(imageResponse) {
+        modelObject['name'] = responseJSON['name'];
+        modelObject['image'] = 'http://pokeapi.co' + imageResponse.image
       });
-      return {
-        name: name,
-        image: image
-      }
+      return modelObject
     });
   }
 });
