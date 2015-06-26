82229a46b40478f6cb5104ee5d5a321bab1f5284
Guest User <student@epicodus.com>
2015-06-26 14:17:00 -0700

    Fix bugs

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('pokedex');
+  this.route('pokemon', {path: 'pokemon/:pkmn_id'});
+
+  this.resource('map', function() {
+    // this.resource('info-window');
+  });
+  this.route('info-window');
 });
 
 export default Router;
